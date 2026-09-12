939d83ce6611404c52307df83453443db0b86efc
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-09-12 15:31:33 -0300

    feat(appointment): wire UserRepository into read use case beans
    Update AppointmentUseCaseConfig to inject UserRepository into
    getAppointmentByIdUseCase and listAppointmentsUseCase.

--- scheduling-service/src/main/java/com/fiap/scheduling_service/medicalAppointment/infrastructure/config/AppointmentUseCaseConfig.java
+++ scheduling-service/src/main/java/com/fiap/scheduling_service/medicalAppointment/infrastructure/config/AppointmentUseCaseConfig.java
@@ -2,6 +2,7 @@ package com.fiap.scheduling_service.medicalAppointment.infrastructure.config;
 
 import com.fiap.scheduling_service.medicalAppointment.application.useCases.*;
 import com.fiap.scheduling_service.medicalAppointment.domain.repository.AppointmentRepository;
+import com.fiap.scheduling_service.user.domain.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,32 +11,32 @@ public class AppointmentUseCaseConfig {
 
 
     @Bean
-    public CreateAppointmentUseCase createAppointmentUseCase(AppointmentRepository appointmentRepository) {
-        return new CreateAppointmentUseCase(appointmentRepository);
+    public CreateAppointmentUseCase createAppointmentUseCase(AppointmentRepository appointmentRepository, UserRepository userRepository) {
+        return new CreateAppointmentUseCase(appointmentRepository, userRepository);
     }
 
     @Bean
-    public EditAppointmentUseCase editAppointmentUseCase(AppointmentRepository appointmentRepository) {
-        return new EditAppointmentUseCase(appointmentRepository);
+    public EditAppointmentUseCase editAppointmentUseCase(AppointmentRepository appointmentRepository, UserRepository userRepository) {
+        return new EditAppointmentUseCase(appointmentRepository, userRepository);
     }
 
     @Bean
-    public CancelAppointmentUseCase cancelAppointmentUseCase(AppointmentRepository appointmentRepository) {
-        return new CancelAppointmentUseCase(appointmentRepository);
+    public CancelAppointmentUseCase cancelAppointmentUseCase(AppointmentRepository appointmentRepository, UserRepository userRepository) {
+        return new CancelAppointmentUseCase(appointmentRepository, userRepository);
     }
 
     @Bean
-    public CompleteAppointmentUseCase completeAppointmentUseCase(AppointmentRepository appointmentRepository) {
-        return new CompleteAppointmentUseCase(appointmentRepository);
+    public CompleteAppointmentUseCase completeAppointmentUseCase(AppointmentRepository appointmentRepository, UserRepository userRepository) {
+        return new CompleteAppointmentUseCase(appointmentRepository, userRepository);
     }
 
     @Bean
-    public GetAppointmentByIdUseCase getAppointmentByIdUseCase(AppointmentRepository appointmentRepository) {
-        return new GetAppointmentByIdUseCase(appointmentRepository);
+    public GetAppointmentByIdUseCase getAppointmentByIdUseCase(AppointmentRepository appointmentRepository, UserRepository userRepository) {
+        return new GetAppointmentByIdUseCase(appointmentRepository, userRepository);
     }
 
     @Bean
-    public ListAppointmentsUseCase listAppointmentsUseCase(AppointmentRepository appointmentRepository) {
-        return new ListAppointmentsUseCase(appointmentRepository);
+    public ListAppointmentsUseCase listAppointmentsUseCase(AppointmentRepository appointmentRepository, UserRepository userRepository) {
+        return new ListAppointmentsUseCase(appointmentRepository, userRepository);
     }
 }
