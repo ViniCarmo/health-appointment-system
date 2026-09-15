ca5c8cb2910c4d4abb34409c0490d3a0589b45bd
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-09-15 03:35:49 -0300

    feat(security): register SpringSecurityAuthenticatedProvider as a Spring Bean Annotate with @Component so it can be injected into use cases needing the current authenticated userr's id.

--- scheduling-service/src/main/java/com/fiap/scheduling_service/shared/security/SpringSecurityAuthenticatedUserProvider.java
+++ scheduling-service/src/main/java/com/fiap/scheduling_service/shared/security/SpringSecurityAuthenticatedUserProvider.java
@@ -2,9 +2,11 @@ package com.fiap.scheduling_service.shared.security;
 
 import com.fiap.scheduling_service.user.infrastructure.security.UserDetailsImpl;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class SpringSecurityAuthenticatedUserProvider implements AuthenticatedUserProvider{
     @Override
     public UUID getLoggedUserId() {
